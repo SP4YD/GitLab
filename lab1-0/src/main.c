cfc6eff082c71c8de31998e02d92529829aeb498
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-02 13:24:35 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -82,6 +82,6 @@ int main(void) {
 		}
 	}*/
 	//Check(0, LenPattern - 1, LenText, LenPattern, pattern, text, imatrix);
-	for (int i = 0; i < 256; ++i) { printf("%c) %d\n", i, cmatrix[i]); } 
+	for (int i = 65; i < 256; ++i) { printf("%c) %d\n", i, cmatrix[i]); } 
 	return 0;
 }
