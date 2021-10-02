79621c16dce73cacd583361795876198787f2aa8
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-02 13:38:47 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-//#include <local.h>
+#include <local.h>
 #define SIZE 256
 
 /*int Rus(int a) {
@@ -14,7 +14,7 @@
 	return a;
 }*/
 
-/*void Check(bool done, int icheck, int LenText, int LenPattern, char *pattern, char *text, int *imatrix){
+void Check(bool done, int icheck, int LenText, int LenPattern, char *pattern, char *text, int *imatrix){
 	
 	while (!done && icheck < LenText) {
 		bool coincidence = 1;
@@ -53,10 +53,10 @@
 		}
 	}
 }
-*/
+
 int main(void) {
-	//setlocale(LC_ALL, "Rus");
-	/*char pattern[SIZE] = "\0", text[SIZE] = "\0", chr;
+	setlocale(LC_ALL, "Rus");
+	char pattern[SIZE] = "\0", text[SIZE] = "\0", chr;
 	int LenText = 0, LenPattern = 0;
 	while ((chr = getc(stdin)) != '\n') {
 		pattern[LenPattern] = chr;
@@ -66,23 +66,22 @@ int main(void) {
 			text[LenText] = chr;
 			++LenText;
 	}
-	if (LenText == 0 && chr == EOF) { return 0; }*/
+	if (LenText == 0 && chr == EOF) { return 0; }
 	//text[LenText - 1] = '\0';
 	//--LenText;
 	char cmatrix[SIZE];
-	//int imatrix[SIZE];
+	int imatrix[SIZE];
 	for (int i = 0; i < 256; ++i) {
 		cmatrix[i] = i;
-		//imatrix[i] = LenPattern;
-	}/*
+		imatrix[i] = LenPattern;
+	}
 	for (int i = LenPattern - 2; i >= 0; --i) {
 		int pattern_now = pattern[i];
 		if (imatrix[pattern_now] == LenPattern) {
 			imatrix[pattern_now] = LenPattern - i - 1;
 		}
-	}*/
+	}
 	//Check(0, LenPattern - 1, LenText, LenPattern, pattern, text, imatrix);
-	printf("a is %d   A is %d\n", 'а', 'А');
-	for (int i = 128; i < 256; ++i) { printf("%c) %d\n", i, cmatrix[i]); } 
+	//for (int i = 65; i < 256; ++i) { printf("%c) %d\n", i, imatrix[i]); } 
 	return 0;
 }
