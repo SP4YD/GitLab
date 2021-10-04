457ff0e74fa6d564c8d1577b5c7869ca202a1a9b
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-04 22:30:21 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -81,10 +81,8 @@ int main(void) {
 			imatrix[pattern_now] = LenPattern - i - 1;
 		}
 	}
-	if (pattern == "абвг")
-	{
-	    for (int i = 148; i < 256; ++i) { printf("%c) %d   %d\n", i, cmatrix[i], imatrix[i]); } 
-	}
+    printf("%l", 'а');
+	//for (int i = 148; i < 256; ++i) { printf("%c) %d   %d\n", i, cmatrix[i], imatrix[i]); } 
 	Check(0, LenPattern - 1, LenText, LenPattern, pattern, text, imatrix);
 	return 0;
 }
