2deb3c3cd523a44030786e79f1ce43ad8c96b66e
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-11 22:53:18 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -7,15 +7,18 @@ int FindWeight(int symbol, int n) {
 	for (int i = 0; i < n; ++i) {
 		a *= 3;
 	}
+	//printf("symbol is %c(%d) a is %d == %d\n", symbol, symbol, a,  ((symbol % 3) * a));
 	return ((symbol % 3) * a);
 }
 
-void Compare(int icheck, int LenPattern, int* pattern, int* text) {
-	bool coincidence = 1;
-	for (int i = 0; (i < LenPattern) && coincidence; ++i) {
-		printf("%d ", icheck + i + 1);
-		if (pattern[i] != text[i + icheck]) {
-			coincidence = 0;
+void Compare(int WeightPattern, int WeightPartText,int icheck, int LenPattern, int* pattern, int* text) {
+	if (WeightPattern == WeightPartText) {
+		bool coincidence = 1;
+		for (int i = 0; (i < LenPattern) && coincidence; ++i) {
+			printf("%d ", icheck + i + 1);
+			if (pattern[i] != text[i + icheck]) {
+				coincidence = 0;
+			}
 		}
 	}
 }
@@ -26,12 +29,12 @@ void Check(int LenText, int LenPattern, int* pattern, int* text, int WeightPatte
 		WeightPartText += FindWeight(text[i], i);
 	}
 	while ((icheck + LenPattern) < LenText) {
-		if (WeightPattern == WeightPartText) {
-			Compare(icheck, LenPattern, pattern, text);
-		}
+		//printf("%d and %d\n", WeightPattern, WeightPartText);
+		Compare(WeightPattern, WeightPartText, icheck, LenPattern, pattern, text);
 		WeightPartText = (WeightPartText - FindWeight(text[icheck], 0)) / 3 + FindWeight(text[icheck + LenPattern], LenPattern - 1);
 		++icheck;
 	}
+	Compare(WeightPattern, WeightPartText, icheck, LenPattern, pattern, text);
 }
 
 int main(void) {
@@ -48,7 +51,7 @@ int main(void) {
 	for (int i = 0; i < LenPattern; ++i) {
 		WeightPattern += FindWeight(pattern[i], i);
 	}
-	printf("\n%d ", WeightPattern);
+	printf("%d ", WeightPattern);
 	Check(LenText, LenPattern, pattern, text, WeightPattern);
 	return 0;
 }
