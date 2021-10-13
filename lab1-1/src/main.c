fdbedd4d4a8c60b02d14870fd212e000c3757985
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-13 19:40:09 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -11,11 +11,11 @@ int FindWeight(int symbol, int n) { /////////////переделать
 	return (((symbol) % 3) * a);
 }
 
-void Compare(int WeightPattern, int WeightPartText, int icheck, int LenPattern, unsigned char* pattern, unsigned char* text) {
+void Compare(int WeightPattern, int WeightPartText, int icheck, int LenPattern, unsigned char* pattern, unsigned char* text, int index) {
 	if (WeightPattern == WeightPartText) {
 		bool coincidence = 1;
 		for (int i = 0; (i < LenPattern) && coincidence; ++i) {
-			printf("%d ", icheck + i + 1);
+			printf("%d ", icheck + i + index);
 			if (pattern[i] != text[i + icheck]) {
 				coincidence = 0;
 			}
@@ -23,7 +23,7 @@ void Compare(int WeightPattern, int WeightPartText, int icheck, int LenPattern, 
 	}
 }
 
-void Check(int LenText, int LenPattern, unsigned char* pattern, unsigned char* text, int WeightPattern) {
+void Check(int LenText, int LenPattern, unsigned char* pattern, unsigned char* text, int WeightPattern, int index) {
 	int WeightPartText = 0, icheck = 0;
 	for (int i = 0; i < LenPattern; ++i) {
 		WeightPartText += FindWeight(text[i], i);
@@ -38,7 +38,7 @@ void Check(int LenText, int LenPattern, unsigned char* pattern, unsigned char* t
 }
 
 int main(void) {
-	int LenText = 0, LenPattern = 0, WeightPattern = 0;
+	int LenText = 0, LenPattern = 0, WeightPattern = 0, index = 1;
 	unsigned char text[SIZE], chr, pattern[16];
 	while ((chr = getchar()) != '\n') {
 		pattern[LenPattern] = chr;
@@ -46,15 +46,17 @@ int main(void) {
 	}
 	LenText = fread(text, 1, SIZE, stdin);
 	if (LenText == 0) { printf("0"); return 0; }
-	/*for (int i = 0; i < LenText; ++i){
-		text[i] = input[i];
-	}*/
-	//printf("text is %s\n", text);
-	//printf("LenText is %d\n",LenText);
 	for (int i = 0; i < LenPattern; ++i) {
 		WeightPattern += FindWeight(pattern[i], i);
 	}
 	printf("%d ", WeightPattern);
-	Check(LenText, LenPattern, pattern, text, WeightPattern);
+	Check(LenText, LenPattern, pattern, text, WeightPattern, index);
+	if (text[LenText - 1] != EOF){
+	    for (int i = 0; text[LenText - 1] != EOF; ++i){
+	        index += LenText;
+	        LenText = fread(text, 1, SIZE, stdin);
+	        Check(LenText, LenPattern, pattern, text, WeightPattern, index);
+	    }
+	}
 	return 0;
 }
