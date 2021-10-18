9f64791bd85ad0e161891bbecfc57f53ce694786
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-18 22:17:55 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -38,8 +38,8 @@ void Check(int lenText, int lenPattern, unsigned char* pattern, unsigned char* t
 int main(void) {
 	int lenText = 0, lenPattern = 0, weightPattern = 0, index = 1, degree = 1;
 	unsigned char text[SIZE], pattern[16];
-	if (scanf("%15[^\n]s", text) != 1) {return 0 ;}
-	for (int i = 0; text[i] != '\n'; ++i) {
+	if (scanf("%15[^\n]s", pattern) != 1) {return 0 ;}
+	for (int i = 0; pattern[i] != '\0'; ++i) {
 		++lenPattern;
 	}
 	lenText = fread(text, 1, SIZE, stdin);
