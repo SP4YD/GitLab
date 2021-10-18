691e7a8c86beaea9f50a8b48c42ec3b7f6e8f06a
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-18 19:36:02 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -5,71 +5,71 @@
 #include <string.h>
 #define SIZE 100000
 
-void Check(bool done, int icheck, int LenText, int LenPattern, unsigned char* pattern, unsigned char* text, int* imatrix, int index) {
+void Check(bool done, int icheck, int lenText, int lenPattern, unsigned char* pattern, unsigned char* text, int* imatrix, int index) {
 
-	while (!done && icheck < LenText) {
+	while (!done && icheck < lenText) {
 		bool coincidence = 1;
 		int i = 0;
 		printf("%d ", icheck + index);
-		if (text[icheck] != pattern[LenPattern - 1]) {
+		if (text[icheck] != pattern[lenPattern - 1]) {
 			coincidence = 0;
-			if (icheck == LenText - 1) {
+			if (icheck == lenText - 1) {
 				done = 1;
 			}
 			else {
 				icheck += imatrix[(int)text[icheck]];
-				if (icheck > LenText - 1) {
-					icheck = LenText - 1;
+				if (icheck > lenText - 1) {
+					icheck = lenText - 1;
 				}
 			}
 		}
-		for (i = 1; i < LenPattern && coincidence; ++i) {
+		for (i = 1; i < lenPattern && coincidence; ++i) {
 			printf("%d ", icheck + index - i);
-			if (text[icheck - i] != pattern[LenPattern - 1 - i]) {
+			if (text[icheck - i] != pattern[lenPattern - 1 - i]) {
 				coincidence = 0;
-				if (icheck == LenText - 1) {
+				if (icheck == lenText - 1) {
 					done = 1;
 				}
 				else {
 					icheck += imatrix[text[icheck]];
-					if (icheck > LenText - 1) {
-						icheck = LenText - 1;
+					if (icheck > lenText - 1) {
+						icheck = lenText - 1;
 					}
 				}
 
 			}
 		}
-		if (i == LenPattern && coincidence) {
-			icheck += imatrix[pattern[LenPattern - 1]];
+		if (i == lenPattern && coincidence) {
+			icheck += imatrix[pattern[lenPattern - 1]];
 		}
 	}
 }
 
 int main(void) {
-	int LenText = 0, LenPattern = 0, index = 1;
+	int lenText = 0, lenPattern = 0, index = 1;
 	unsigned char text[SIZE], chr, pattern[16];
 	while ((chr = getchar()) != '\n') {
-		pattern[LenPattern] = chr;
-		++LenPattern;
+		pattern[lenPattern] = chr;
+		++lenPattern;
 	}
-	LenText = fread(text, 1, SIZE, stdin);
-	if (LenText == 0) { return 0; }
+	lenText = fread(text, 1, SIZE, stdin);
+	if (lenText == 0) { return 0; }
 	int imatrix[SIZE];
 	for (int i = 0; i < SIZE; ++i) {
-		imatrix[i] = LenPattern;
+		imatrix[i] = lenPattern;
 	}
-	for (int i = LenPattern - 2; i >= 0; --i) {
+	for (int i = lenPattern - 2; i >= 0; --i) {
 		int PatternNow = pattern[i];
-		if (imatrix[PatternNow] == LenPattern) {
-			imatrix[PatternNow] = LenPattern - i - 1;
+		if (imatrix[PatternNow] == lenPattern) {
+			imatrix[PatternNow] = lenPattern - i - 1;
 		}
 	}
-	Check(0, LenPattern - 1, LenText, LenPattern, pattern, text, imatrix, index);
-	if (LenText == SIZE) {
-		while (LenText == SIZE) {
-			index += LenText;
-			LenText = fread(text, 1, SIZE, stdin);
-			Check(0, LenPattern - 1, LenText, LenPattern, pattern, text, imatrix, index);
+	Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
+	if (lenText == SIZE) {
+		while (lenText == SIZE) {
+			index += lenText;
+			lenText = fread(text, 1, SIZE, stdin);
+			Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
 		}
 	}
 	return 0;
