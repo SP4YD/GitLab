79fdc38bdf5dec3002a00438666fc95c6551c8b8
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-28 21:22:02 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -1,28 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
 
 int FindWeight(int symbol, int degree) {
 	return (((symbol) % 3) * degree);
 }
 
-void Compare(int weightPattern, int weightPartText, int lenPattern, unsigned char* pattern, unsigned char* text, int index) {
-	if (weightPattern == weightPartText) {
+int Check(int lenPattern, unsigned char* pattern, unsigned char* text, int weightPattern, int weightText, int index) {
+	if (weightPattern == weightText) {
 		for (int i = 0; i < lenPattern; ++i) {
 			printf("%d ", i + index);
 			if (pattern[i] != text[i]) {
-				return;
+				break;
 			}
 		}
 	}
-}
-
-int Check(int lenPattern, unsigned char* pattern, unsigned char* text, int weightPattern, int weightText, int index) {
-	Compare(weightPattern, weightText, lenPattern, pattern, text, index);
 	weightText = (weightText - FindWeight(text[0], 1)) / 3;
 	return weightText;
 }
@@ -38,9 +31,7 @@ int main(void) {
 	int lenText = 0, lenPattern = 0, weightPattern = 0, index = 0, degree = 1, weightText = 0;
 	unsigned char pattern[17];
 	if (scanf("%16[^\n]s", pattern) != 1) { return 0; }
-	for (int i = 0; pattern[i] != '\0'; ++i) {
-		++lenPattern;
-	}
+	lenPattern = strlen((char*)pattern);
 	int degreeLastSymb = pow(3, lenPattern - 1);
 	unsigned char text[17];
 	lenText = fread(text, 1, lenPattern, stdin);
@@ -54,14 +45,12 @@ int main(void) {
 	weightText = Check(lenPattern, pattern, text, weightPattern, weightText, index);
 	unsigned char newText[1];
 	lenText = fread(newText, 1, 1, stdin);
-	if (lenText) {
-		while (lenText) {
-			Shift(text, lenPattern, newText[0]);
-			weightText += FindWeight(newText[0], degreeLastSymb);
-			index += 1;
-			weightText = Check(lenPattern, pattern, text, weightPattern, weightText, index);
-			lenText = fread(newText, 1, 1, stdin);
-		}
+	while (lenText) {
+		Shift(text, lenPattern, newText[0]);
+		weightText += FindWeight(newText[0], degreeLastSymb);
+		++index;
+		weightText = Check(lenPattern, pattern, text, weightPattern, weightText, index);
+		lenText = fread(newText, 1, 1, stdin);
 	}
 	return 0;
 }
