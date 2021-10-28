83070ab8ecd772d45f6fe6aec0ba85965cb06383
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-28 22:57:15 +0700

    Update main.c

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -15,7 +15,7 @@ int Compare(int lenPattern, unsigned char* pattern, unsigned char* text, int wei
 			}
 		}
 	}
-	return weightText - FindWeight(text[0], 1)) / 3;
+	return (weightText - FindWeight(text[0], 1)) / 3;
 }
 
 void Shift(unsigned char* text, int lenPattern, unsigned char symbol) {
@@ -36,6 +36,29 @@ void Check(int lenPattern, int lenText, unsigned char* text, unsigned char* patt
 	weightText = Compare(lenPattern, pattern, text, weightPattern, weightText, index);
 	unsigned char newText[1];
 	lenText = fread(newText, 1, 1, stdin);
+	while (lenText) {
+		Shift(text, lenPattern, newText[0]);
+		weightText += FindWeight(newText[0], degreeLastSymb);
+		++index;
+		weightText = Compare(lenPattern, pattern, text, weightPattern, weightText, index);
+		lenText = fread(newText, 1, 1, stdin);
+	}
+}
+
+int main(void) {
+	int lenText, lenPattern = 0;
+	unsigned char pattern[17];
+	if (scanf("%16[^\n]s", pattern) != 1) { return 0; }
+	for (int i = 0; pattern[i] != '\0'; ++i) {
+		++lenPattern;
+	}
+	unsigned char text[17];
+	lenText = fread(text, 1, lenPattern, stdin);
+	if (lenText == 0 || lenText < lenPattern) { printf("0"); return 0; }
+	Check(lenPattern, lenText, text, pattern);
+	return 0;
+}
+
 	while (lenText) {
 		Shift(text, lenPattern, newText[0]);
 		weightText += FindWeight(newText[0], degreeLastSymb);
