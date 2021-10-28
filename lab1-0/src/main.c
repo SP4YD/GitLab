5c5b61b748a1ad946e77fc47168f85ea152842a8
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-28 23:08:53 +0700

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -1,76 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#define SIZE 100000
+#include <math.h>
 
-void Check(bool done, int icheck, int lenText, int lenPattern, unsigned char* pattern, unsigned char* text, int* imatrix, int index) {
-
-	while (!done && icheck < lenText) {
-		bool coincidence = 1;
-		int i = 0;
-		printf("%d ", icheck + index);
-		if (text[icheck] != pattern[lenPattern - 1]) {
-			coincidence = 0;
-			if (icheck == lenText - 1) {
-				done = 1;
-			}
-			else {
-				icheck += imatrix[(int)text[icheck]];
-				if (icheck > lenText - 1) {
-					icheck = lenText - 1;
-				}
+int Compare(int lenPattern, unsigned char* pattern, unsigned char* text, int weightPattern, int weightText, int index) {
+	if (weightPattern == weightText) {
+		for (int i = 0; i < lenPattern; ++i) {
+			printf("%d ", i + index);
+			if (pattern[i] != text[i]) {
+				break;
 			}
 		}
-		for (i = 1; i < lenPattern && coincidence; ++i) {
-			printf("%d ", icheck + index - i);
-			if (text[icheck - i] != pattern[lenPattern - 1 - i]) {
-				coincidence = 0;
-				if (icheck == lenText - 1) {
-					done = 1;
-				}
-				else {
-					icheck += imatrix[text[icheck]];
-					if (icheck > lenText - 1) {
-						icheck = lenText - 1;
-					}
-				}
+	}
+	return (weightText - (((text[0]) % 3) * 1)) / 3;
+}
 
-			}
-		}
-		if (i == lenPattern && coincidence) {
-			icheck += imatrix[pattern[lenPattern - 1]];
-		}
+void Shift(unsigned char* text, int lenPattern, unsigned char symbol) {
+	for (int i = 0; i < lenPattern - 1; ++i) {
+		text[i] = text[i + 1];
+	}
+	text[lenPattern - 1] = symbol;
+}
+
+void Check(int lenPattern, int lenText, unsigned char* text, unsigned char* pattern) {
+	int weightPattern = 0, index = 0, degree = 1, weightText = 0, degreeLastSymb = pow(3, lenPattern - 1);;
+	for (int i = 0; i < lenPattern; ++i) {
+		weightText += ((text[i]) % 3) * degree;
+		weightPattern += ((pattern[i]) % 3) * degree;
+		degree *= 3;
+	}
+	printf("%d ", weightPattern);
+	weightText = Compare(lenPattern, pattern, text, weightPattern, weightText, index);
+	unsigned char newText[1];
+	lenText = fread(newText, 1, 1, stdin);
+	while (lenText) {
+		weightText += ((newText[0]) % 3) * degreeLastSymb;
+		++index;
+		Shift(text, lenPattern, newText[0]);
+		weightText = Compare(lenPattern, pattern, text, weightPattern, weightText, index);
+		lenText = fread(newText, 1, 1, stdin);
 	}
 }
 
 int main(void) {
-	int lenText = 0, lenPattern = 0, index = 1;
-	unsigned char text[SIZE], chr, pattern[16];
-	while ((chr = getchar()) != '\n') {
-		pattern[lenPattern] = chr;
+	int lenText, lenPattern = 0;
+	unsigned char pattern[17];
+	if (scanf("%16[^\n]s", pattern) != 1) { return 0; }
+	for (int i = 0; pattern[i] != '\0'; ++i) {
 		++lenPattern;
 	}
-	lenText = fread(text, 1, SIZE, stdin);
-	if (lenText == 0) { return 0; }
-	int imatrix[SIZE];
-	for (int i = 0; i < SIZE; ++i) {
-		imatrix[i] = lenPattern;
-	}
-	for (int i = lenPattern - 2; i >= 0; --i) {
-		int PatternNow = pattern[i];
-		if (imatrix[PatternNow] == lenPattern) {
-			imatrix[PatternNow] = lenPattern - i - 1;
-		}
-	}
-	Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
-	if (lenText == SIZE) {
-		while (lenText == SIZE) {
-			index += lenText;
-			lenText = fread(text, 1, SIZE, stdin);
-			Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
-		}
-	}
+	unsigned char text[17];
+	lenText = fread(text, 1, lenPattern, stdin);
+	if (lenText == 0 || lenText < lenPattern) { printf("0"); return 0; }
+	Check(lenPattern, lenText, text, pattern);
 	return 0;
 }
