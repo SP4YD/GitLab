e4316bb02667ddba7a916622190c0b95f7a72eb9
n.yurakov <n.yurakov@g.nsu.ru>
2021-10-31 20:56:52 +0700

    Update main.c

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -1,3 +1,4 @@
+#include "pch.h"
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,75 +7,73 @@
 #define SIZE 1000
 #define Swap(a, b) (((a) ^= (b)), ((b) ^= (a)), ((a) ^= (b)))
 
-void ConvertInChislo(unsigned int* number, int lenNum) {
-    for (int i = 0; i < lenNum; ++i) {
-        number[i] -= '0';
-    }
+int Chislo(unsigned char symbol) {
+	return symbol - '0';
 }
 
-void Chek(unsigned int number[SIZE], int lenNum) {
-    if (lenNum < 11){
-        int coincidence = 1;
-        char repeat[10] = { 0 };
-        for (int i = 0; i < lenNum; ++i) {
-           if (!repeat[number[i]] && number[i] <= 9 && number[i] >= 0){
-               repeat[number[i]] = 1;
-           }
-           else{
-               printf("bad input");
-               exit(0);
-           }
-           if (i > 0 && number[i] > number[i - 1] && coincidence) {
-                coincidence = 0;
-            }
-        }
-        if (coincidence) {
-            exit(0);
-        }
-    }
+void Chek(unsigned char number[SIZE], int lenNum) {
+	if (lenNum < 11) {
+		int coincidence = 1;
+		char repeat[10] = { 0 };
+		for (int i = 0; i < lenNum; ++i) {
+			if (!repeat[Chislo(number[i])] && number[i] <= '9' && number[i] >= '0') {
+				repeat[Chislo(number[i])] = 1;
+			}
+			else {
+				printf("bad input");
+				exit(0);
+			}
+			if (i > 0 && number[i] > number[i - 1] && coincidence) {
+				coincidence = 0;
+			}
+		}
+		if (coincidence) {
+			exit(0);
+		}
+	}
 }
 
-void PartialTransformations(unsigned int* number, int lenNum) {
-    int i = lenNum - 2, j;
-    while (number[i] > number[i + 1]) {
-        --i;
-    }
-    unsigned int minNum = 10, mini = 0;
-    j = i + 1;
-    while (j < lenNum) {
-        if ((number[j] < minNum) && (number[j] > number[i])) {
-            minNum = number[j];
-            mini = j;
-        }
-        ++j;
-    }
-    Swap(number[i], number[mini]);
-    for (int z = i + 1; z < (lenNum  + i) / 2; ++z) {
-        Swap(number[z], number[lenNum - z + i]);
-    }
+void PartialTransformations(unsigned char* number, int lenNum) {
+	int i = lenNum - 2, j;
+	while (number[i] > number[i + 1]) {
+		--i;
+	}
+	unsigned int minNum = '9' + 1, mini = 0;
+	j = i + 1;
+	while (j < lenNum) {
+		if ((number[j] < minNum) && (number[j] > number[i])) {
+			minNum = number[j];
+			mini = j;
+		}
+		++j;
+	}
+	Swap(number[i], number[mini]);
+	for (int z = i + 1; z < (lenNum + i) / 2; ++z) {
+		Swap(number[z], number[lenNum - z + i]);
+	}
 }
 
-void GeneralTransformations(unsigned int* number, int lenNum, int numOfTurns) {
-    for (int i = 0; i < numOfTurns; ++i) {
-        PartialTransformations(number, lenNum);
-        for (int i = 0; i < lenNum; ++i) { printf("%u", number[i]); }
-        Chek(number, lenNum);
-        if (i != numOfTurns - 1) {
-            printf("\n");
-        }
-    }
+void GeneralTransformations(unsigned char* number, int lenNum, int numOfTurns) {
+	for (int i = 0; i < numOfTurns; ++i) {
+		PartialTransformations(number, lenNum);
+		printf("%s", number);
+		Chek(number, lenNum);
+		if (i != numOfTurns - 1) {
+			printf("\n");
+		}
+	}
 }
 
 int main(void) {
-    unsigned int number[SIZE];
-    unsigned int lenNum = 0, numOfTurns = 0, chr; // lenNOT = lenNumOfTurns
-    while ((chr = getchar()) != '\n') {
-        number[lenNum] = chr;
-        ++lenNum;
-    }
-    if (scanf("%u", &numOfTurns) != 1) { return 0; }
-    ConvertInChislo(number, lenNum);
-    Chek(number, lenNum);
-    GeneralTransformations(number, lenNum, numOfTurns);
-    return 0;
+	unsigned char number[SIZE];
+	unsigned int lenNum = 0, numOfTurns = 0, chr; // lenNOT = lenNumOfTurns
+	while ((chr = getchar()) != '\n') {
+		number[lenNum] = chr;
+		++lenNum;
+	}
+	number[lenNum] = '\0';
+	if (scanf("%u", &numOfTurns) != 1) { return 0; }
+	Chek(number, lenNum);
+	GeneralTransformations(number, lenNum, numOfTurns);
+	return 0;
 }
