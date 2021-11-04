47d5b25239c8e2b1363bc4830b4afc55664803ca
n.yurakov <n.yurakov@g.nsu.ru>
2021-11-05 00:05:21 +0700

    Update main.c

--- lab3-0/src/main.c
+++ lab3-0/src/main.c
@@ -1,5 +1,60 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define swap(a, b) (((a) ^= (b)), ((b) ^= (a)), ((a) ^= (b)))
 
-int main(void) {
-    return EXIT_SUCCESS;
+void ExchangeDown(int* iArray, int lenArray, int index);
+
+void ExchangeUp(int* iArray, int lenArray, int index) {
+	if ((index / 2) > 0) {
+		if (iArray[index] > iArray[index / 2]) {
+			swap(iArray[index], iArray[index / 2]);
+			ExchangeDown(iArray, lenArray, index);
+			ExchangeUp(iArray, lenArray, index/2);
+		}
+	}
 }
+
+void ExchangeDown(int* iArray, int lenArray, int index) {
+	if ((index * 2) < lenArray){
+		int max = index * 2;
+		if ((index * 2 + 1) < lenArray && iArray[index * 2] < iArray[index * 2 + 1]) {
+			++max;
+		}
+		if (iArray[max] > iArray[index]) {
+			swap(iArray[index], iArray[max]);
+			ExchangeDown(iArray, lenArray, max);
+			ExchangeUp(iArray, lenArray, index);
+		}
+	}
+}
+
+void ChangeToMaxHeap(int* iArray, int lenArray) {
+	for (int i = 1; i * 2 <= lenArray; ++i) {
+		ExchangeDown(iArray, lenArray, i);
+	}
+}
+
+void HeapSort(int* iArray, int lenArray) {
+	while (lenArray > 2) {
+		swap(iArray[1], iArray[lenArray - 1]);
+		--lenArray;
+		ChangeToMaxHeap(iArray, lenArray);
+	}
+}
+
+int main(void){
+	int lenArray;
+	if (scanf("%d", &lenArray) != 1) { return 0 };
+	++lenArray;
+	int* iArray = (int*)malloc(sizeof(int) * lenArray);
+	for (int i = 1; i < lenArray; ++i) {
+		if (scanf("%d", &iArray[i]) != 1) { return 0 };
+	}
+	ChangeToMaxHeap(iArray, lenArray);
+	HeapSort(iArray, lenArray);
+	for (int i = 1; i < lenArray; ++i) {
+		printf("%d ", iArray[i]);
+	}
+}
+
