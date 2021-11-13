4a0cea69e04747c4544be142c92f73801ec0c286
n.yurakov <n.yurakov@g.nsu.ru>
2021-11-13 10:43:45 +0000

    Update main.c

--- lab1-0/src/main.c
+++ lab1-0/src/main.c
@@ -3,68 +3,59 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#define SIZE 100000
 
-void Check(
-	bool done,
-	int icheck, 
-	int lenText, 
-	int lenPattern, 
-	unsigned char* pattern, 
-	unsigned char* text, 
-	int* imatrix, 
+int Check(
+	int lenPattern,
+	unsigned char* pattern,
+	unsigned char* text,
+	int* imatrix,
 	int index
-	) {
-
-	while (!done && icheck < lenText) {
-		bool coincidence = 1;
-		int i = 0;
-		printf("%d ", icheck + index);
-		if (text[icheck] != pattern[lenPattern - 1]) {
-			coincidence = 0;
-			if (icheck == lenText - 1) {
-				done = 1;
-			}
-			else {
-				icheck += imatrix[(int)text[icheck]];
-				if (icheck > lenText - 1) {
-					icheck = lenText - 1;
-				}
-			}
-		}
-		for (i = 1; i < lenPattern && coincidence; ++i) {
-			printf("%d ", icheck + index - i);
-			if (text[icheck - i] != pattern[lenPattern - 1 - i]) {
-				coincidence = 0;
-				if (icheck == lenText - 1) {
-					done = 1;
-				}
-				else {
-					icheck += imatrix[text[icheck]];
-					if (icheck > lenText - 1) {
-						icheck = lenText - 1;
-					}
-				}
-
-			}
-		}
-		if (i == lenPattern && coincidence) {
-			icheck += imatrix[pattern[lenPattern - 1]];
+) {
+	int icheck = lenPattern - 1;
+	printf("%d ", icheck + index);
+	if (text[icheck - 1] != pattern[icheck - 1]) {
+		return imatrix[(int)text[icheck]];
+	}
+	for (int i = lenPattern - 2; i > 0; --i) {
+		printf("%d ", i + index );
+		if (text[i] != pattern[i]) {
+			return imatrix[(int)text[icheck]];
 		}
 	}
+	return 1;
+}
+
+void Shift(unsigned char* text, int shift, int len) {
+	memmove(text, text + (shift * sizeof(unsigned char)), len - shift);
+}
+
+int Upadte(unsigned char* text, int shift, int len) {
+	if (shift != len) {
+		Shift(text, shift, len);
+	}
+	return fread(
+		text + ((shift * sizeof(unsigned char)) % len),
+		sizeof(unsigned char),
+		shift, stdin);
 }
 
 int main(void) {
-	int lenText = 0, lenPattern = 0, index = 1;
-	unsigned char text[SIZE], chr, pattern[16];
+	int lenPattern = 0, index = 1;
+	unsigned char chr, pattern[16];
+
 	while ((chr = getchar()) != '\n') {
 		pattern[lenPattern] = chr;
 		++lenPattern;
 	}
-	lenText = fread(text, 1, SIZE, stdin);
+
+	unsigned char* text = (unsigned char*) malloc(sizeof(unsigned char) * (lenPattern));
+	int lenText = fread(text, sizeof(unsigned char), lenPattern, stdin);
+
 	if (lenText == 0) { return 0; }
-	int imatrix[SIZE];
-	for (int i = 0; i < SIZE; ++i) {
+
+	int imatrix[256];
+
+	for (int i = 0; i < 256; ++i) {
 		imatrix[i] = lenPattern;
 	}
 	for (int i = lenPattern - 2; i >= 0; --i) {
@@ -73,11 +64,15 @@ int main(void) {
 			imatrix[PatternNow] = lenPattern - i - 1;
 		}
 	}
-	Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
-	while (lenText == SIZE) {
-			index += lenText;
-			lenText = fread(text, 1, SIZE, stdin);
-			Check(0, lenPattern - 1, lenText, lenPattern, pattern, text, imatrix, index);
+
+	int shift = Check(lenPattern, pattern, text, imatrix, index);
+	int lenRead = Upadte(text, shift, lenPattern);
+
+	while (lenRead) {
+		index += lenRead;
+		shift = Check(lenPattern, pattern, text, imatrix, index);
+		lenRead = Upadte(text, shift, lenPattern);
 	}
+
 	return 0;
 }
