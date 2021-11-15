b8b0593ab51e052a6230d27672220eb7794cec47
n.yurakov <n.yurakov@g.nsu.ru>
2021-11-15 16:04:53 +0000

    Update main.c

--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,5 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define swap(a, b) (((a) ^= (b)), ((b) ^= (a)), ((a) ^= (b)))
+
+int QuickSort(int* iArray, int left , int right) {
+	if (left < right) {
+
+		int iPivot = right;
+		int wall = left;
+
+		for (int i = left; i <= right; ++i) {
+			if (iArray[i] < iArray[iPivot]) {
+				if (i != wall) {
+					swap(iArray[i], iArray[wall]);
+				}
+				++wall;
+			}
+		}
+
+		if (iPivot != wall) {
+			swap(iArray[iPivot], iArray[wall]);
+		}
+
+		QuickSort(iArray, left, wall - 1);
+		QuickSort(iArray, wall + 1, right);
+	}
+}
 
 int main(void) {
-    return EXIT_SUCCESS;
+	int lenArray;
+
+	if (scanf("%d", &lenArray) != 1) { return 0; }
+
+	int* iArray = (int*)malloc(sizeof(int) * lenArray);
+
+	for (int i = 0; i < lenArray; ++i) {
+		if (scanf("%d", &iArray[i]) != 1) { return 0; }
+	}
+
+	QuickSort(iArray, 0, lenArray - 1);
+
+	for (int i = 0; i < lenArray; ++i) {
+		printf("%d ", iArray[i]);
+	}
+
+	free(iArray);
+
+	return 0;
 }
