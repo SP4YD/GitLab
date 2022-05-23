43c04f935cc856f447080b7c16b069c276f13a25
n.yurakov <n.yurakov@g.nsu.ru>
2022-05-23 15:13:55 +0000

    Update main.c

--- lab5/src/main.c
+++ lab5/src/main.c
@@ -392,7 +392,7 @@ int main(void) {
     unsigned char inputSymbol;
     unsigned char task = 0;
 
-    freopen("in.txt", "rb", stdin); freopen("out.txt", "wb", stdout);
+    //freopen("in.txt", "rb", stdin); freopen("out.txt", "wb", stdout);
     //char a = 0, b = 1; fprintf (stdout, "c"); 
     //fprintf (stdout, "%c", '\n');
     //fprintf (stdout, "%c", '\r');
